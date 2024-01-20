8f5248e5d18c0da570de1247e0f5990a0095c310
tiwvidurpals <tiwvidurpals@gmail.com>
2024-01-20 16:28:00 -0700

    update

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <button>Today</button> <button>Tomorrow</button> <button>Date</button>
+      <button>Today</button> <button>Tomorrow</button>{" "}
+      <button>Day after Tomorrow</button>
     </div>
   );
 };
