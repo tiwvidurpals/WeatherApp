2d167aa34cb4ca7db5d5a8f7aa966ea3a1b0cbb3
Vidur Tiwari <111106845+tiwvidurpals@users.noreply.github.com>
2024-05-31 08:42:38 +0545

    Day after Tomorrow assigned the retrieved Date from the API converting it to a week day

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,20 +1,40 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import useWeatherForecast from "../../hooks/useWeatherForecast";
 import { WeatherContext } from "../../utils/weatherContext";
 
 const Navbar = () => {
   const { setDay } = useContext(WeatherContext);
   const { weather } = useWeatherForecast();
+  const [dateAfterTomorrow, setDateAfterTomorrow] = useState('')
 
   
 
   useEffect(()=>{
     if (weather){
-      console.log(weather);
-      const dateDayAfterTomorrow = weather.forecast.forecastday[2].date;
-      console.log(dateDayAfterTomorrow)
+      try{  
+         const date = weather.forecast.forecastday[2].date;
+        // console.log(date);
+        setDateAfterTomorrow(date);
+      } catch(error){
+        console.error('Error fetching the date from the API', error)
+      }
+   
     }
   })
+
+  function getDayOfWeek(dateString: string): string {
+    const date = new Date(dateString);
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return daysOfWeek[date.getUTCDay()];
+  }
+  
+  const dayAfterTomorrow = getDayOfWeek;
+  
+  const dayOfWeek = dateAfterTomorrow ? dayAfterTomorrow(dateAfterTomorrow) : '';
+  console.log(dayOfWeek); // This will log the day of the week for the date after tomorrow
+  
+
+
 
   return (
     <div className="flex flex-wrap justify-evenly bg-blue-400 p-1">
@@ -31,10 +51,10 @@ const Navbar = () => {
         Tomorrow
       </button>
       <button
-        className="text-center text-lg text-yellow-200 h-10 w-64 bg-slate-400 rounded-md shadow-md hover:bg-slate-500 m-2"
+        className="text-center text-lg text-yellow-200 h-10 w-32 bg-slate-400 rounded-md shadow-md hover:bg-slate-500 m-2"
         onClick={() => setDay("dayAfterTommorrow")}
       >
-        Day After Tomorrow
+        {dayOfWeek}
       </button>
     </div>
   );
